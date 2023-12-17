3adf8f1814fbc98ca734e2a04810c056a7e61c5f
selelel <136282944+selelel@users.noreply.github.com>
2023-12-17 13:46:13 +0800

    [CHORE]: Update type for the dispatch comments

--- src/Store/Slices/Comment.ts
+++ src/Store/Slices/Comment.ts
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type Comment = {
+  comment: string[];
+};
+
 const CommentSlice = createSlice({
   name: "comment",
   initialState: {
     comment: [],
-  },
+  } as Comment,
   reducers: {
     Import_Comments(state, action) {
-      console.log("Import", state, action);
+      state.comment = [...state.comment, action.payload];
+      console.log("Import", state.comment);
     },
     Delete_Comments(state, action) {
       console.log("Delete", state, action);
